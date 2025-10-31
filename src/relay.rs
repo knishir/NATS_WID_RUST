62aba5e551f631ab13b0bd1f901764fada985fef
abuzada <nishirkarmakar@gmail.com>
2025-10-31 12:51:58 +0530

    Updaed the relay.rs to setup the resonse for the request it will take.

--- src/relay.rs
+++ src/relay.rs
@@ -1,42 +1,23 @@
 use futures::StreamExt;
 use std::env;
-use std::str::from_utf8; 
-use std::time::Duration;
-
 
 #[tokio::main]
 async fn main() -> Result<(), async_nats::Error> {
     let nats_url = env::var("NATS_URL")
-    .unwrap_or_else(|_| "nats://localhost:4222".to_string());
+        .unwrap_or_else(|_| "nats://localhost:4222".to_string());
 
     let client = async_nats::connect(nats_url).await?;
-    
-    let mut requests = client.subscribe("MICHALE.*").await.unwrap();
 
-    tokio::spawn({
-        let client = client.clone();
-        async move {
-            while let Some(request) = requests.next()
-            .await {
-                if let Some(reply) = request.reply {
-                    let name = &request.subject[6..];
-                    client.publish(reply, format!("HEY, {}", name).into()).await?;
-                }
-            }
-            Ok::<(), async_nats::Error>(())
+    let mut requests = client.subscribe("TALWIINDER.*").await.unwrap();
+
+    while let Some(request) = requests.next().await {
+        if let Some(reply) = request.reply {
+            let name = &request.subject[6..];
+            client
+                .publish(reply, format!("hello, {}", name).into())
+                .await?;
         }
-    });
+    }
 
-    let response = client.request("MICHALE.JORDON", "".into()).await?;
-    println!("GOT A RESPONSE: {:?}", from_utf8(&response.payload)?);
-    
-    let response = client.request("MICHALE.JAIGIRI","".into()).await?;
-    println!("RECEIVED YOUR RESPONSE: {:?}", from_utf8(&response.payload)?);
-
-    let response = tokio::time::timeout(
-        Duration::from_millis(500),client.request("MICHALE.UNKNOWN","".into()),)
-        .await??;
-    println!("GOT A RESPONSE: {:?}", from_utf8(&response.payload)?);
     Ok(())
-
 }
